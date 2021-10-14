f0031a327d39228124d40adb702f69d1e49a6a9e
Safidi <alphonso@web-premiere.fr>
2021-10-14 14:56:35 +0300

    redirect auth component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router,Switch,Route,Link,useParams} from "react-router-dom";
 import ParamsRoute from './parameters-url/url-parameters';
 import ParamsRoute from './nesting/nesting';
+import Auth from './redirect/redirect-auth';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1>Router Cours</h1>
       <ParamsRoute/>
       <Nesting />
+      <Auth />
     </div>
   );
 }
